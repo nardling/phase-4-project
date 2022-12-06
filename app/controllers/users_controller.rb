6331d213ba363437b305ee7fd7dc7d81ddedfb26
Joe Robbins <joe.robbins.cpt@gmail.com>
2022-12-06 13:24:40 -0500

    added login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    # gets all users
+    def index
+    users = User.all
+    render json: users, status: :ok
+    end
+
+    # gets individual user, error if not found
+    def show
+        # byebug
+    user = User.find(params[:id])
+    render json: user, status: :ok, include: [:posts]
+    end
 end
