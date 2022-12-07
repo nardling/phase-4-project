05f20bbd077784d1208e6ccdef3fb3360d079f73
Joe Robbins <joe.robbins.cpt@gmail.com>
2022-12-06 19:16:18 -0500

    fixed logout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # resources :posts
+  resources :posts, only: [:index, :show]
   # resources :user_followers
   resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- flatiron-social/src/Components/Home.js
+++ flatiron-social/src/Components/Home.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from 'react-router-dom';
-function Home(props) {
+import {useHistory} from 'react-router-dom'
 
-    const {setCurrentUser} = props
+
+
+
+const postUrl = 'http://localhost:3000/posts/'
+
+
+
+
+function Home(props) {
+    const {setCurrentUser, currentUser, setErrors} = props
+
+
+    const history = useHistory()
+    const [posts, setPosts] = useState([])
+
     // successful server side, nothing happening client side yet..
     const handleLogout = () => {
         fetch('http://localhost:3000/logout', {
         method: 'DELETE'
         })
-        
+        .then (res => {
+            if(res.ok){
+                history.push('/')
+            } else {
+                res.json()
+                .then(json => setErrors(json.errors))
+            }
+        })
         }
+
+        // Fetches posts associated with logged-in user
+        useEffect(() => {
+            fetch(postUrl + `${currentUser.id}`)
+            .then(res => res.json())
+            .then(setPosts)
+        },[])
+
+console.log(posts)
+
     return <div>
         <h1>Welcome To Flatiron Social</h1>
         {/* <Link onClick={handleLogout} to="/">Click here to go to home</Link> */}
-        <button onClick={handleLogout}>logout</button>
+        <button onClick={handleLogout} className="logout-button">Logout</button>
+        {/* <PostContainer /> */}
         </div>
 }
 
